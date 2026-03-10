b71ccdd14da7cdba40bb5a78c1ea0d49c9d73bc0
Zawee6 <weiweixx921026@gmail.com>
2026-03-10 16:25:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ loader.load('headcopy.glb', (gltf) => {
     
     // 3. 💡 設定位置到「右上角」
     // (數值可以等大小正常後再來微調)
-    headPivot.position.set(20, 12, 0); 
+    headPivot.position.set(40, 12, 0); 
     
     // 4. 💡 關鍵修正：抵銷雙重放大，精準設定大小！
     const maxDim = Math.max(size.x, size.y, size.z);
